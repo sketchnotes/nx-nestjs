e5a4da06ee9ce3cbb4aa86c60c794e5d60c53922
Popov Denys <popovdenys@yahooo.fr>
2020-06-29 18:25:00 +0300

    Remove auth

--- libs/auth/src/lib/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-
-@Module({
-  controllers: [],
-  providers: [AuthService],
-  exports: [AuthService],
-})
-export class AuthModule {}
